93a8299f9c2f0841fc66b87d7544e98459b858eb
Emlice <xiaomozyw@sina.com>
2020-04-28 17:31:05 +0800

    对撞指针解有序数组的两数之和

--- 167.两数之和Ⅱ-输入有序数组.js
+++ 167.两数之和Ⅱ-输入有序数组.js
@@ -34,3 +34,26 @@ var twoSum = function(numbers, target) {
         }
     }
 };
+
+
+/** 对撞指针
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    const len = numbers.length;
+    if (len < 2) return [];
+    let left = 0,
+        right = len - 1;
+    while (left < right) {
+        if (numbers[left] + numbers[right] === target) {
+            return [left + 1, right + 1]
+        } else if (numbers[left] + numbers[right] > target) {
+            right --
+        } else {
+            left ++
+        }
+    }
+    return []
+};
